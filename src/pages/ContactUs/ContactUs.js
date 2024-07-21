5030539de720811460fce7d0080424806bc9f1dd
opaller91 <rinrada.ti@ku.th>
2024-07-21 13:05:24 +0700

    [WIP] Contect-Us Page
    80%

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,109 +1,96 @@
 import React from 'react';
-import '../../App.css';
-import { SlLocationPin } from "react-icons/sl";
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import './ContactUs.css';
+import { SlArrowRight } from "react-icons/sl";
 import { CiPhone } from "react-icons/ci";
 import { CiMail } from "react-icons/ci"; // Import the header image
 
 function ContactUs() {
-    const submitButtonStyle = {
-        backgroundColor: 'white', // Gray with 80% opacity
-        color: 'black',
-        border: '1.5px solid black',
-        padding: '10px 20px',
-        borderRadius: '5px',
-        cursor: 'pointer',
-        fontWeight: 'bold'
-    };
+    const handleSubmit = (event) => { };
 
     return (
-        <div className="ContactUs font-montserrat bg-white min-h-screen relative">
-            {/* Header */}
-            <header className="ContactUs-header flex justify-center items-center relative" style={{ background: 'linear-gradient(to bottom, rgba(0,0,0,1), rgba(255,255,255,0))' }}>
-                <div style={{ position: 'relative', width: '98%', height: '350px', overflow: 'hidden' }}>
-                    <img src='/assets/images/ContactUsHead.jpeg' alt="Header Pic" className='mt-2' style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block'}} />
-                    <h1 className="absolute bottom-0 right-0 text-white text-6xl font-bold mr-8 mb-4">Contact Us</h1>
+        <div className="bg-black text-white font-montserrat min-h-screen min-w-screen p-5">
+            <div>
+                <div className="text-header mb-4 left-aligned ml-8">
+                    <label style={{ fontSize: '72px' }}>PAPER PLANE PROJECT STUDIO CO, LTD.</label>
                 </div>
-            </header>
-            
-            {/* Contact Information and Form */}
-            <div className="max-w-4xl mx-auto mt-8">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                    {/* Contact Information */}
-                    <div className="bg-gray-100 rounded-lg shadow-lg p-6 mt-10 mb-16">
-                        <div className="flex items-center mb-4"> 
-                            <SlLocationPin className="text-4xl text-gray-700 mr-4" />
-                            <div>
-                                <h3 className="text-xl font-semibold text-gray-900">Paper Plane Project Studio Limited</h3>
-                                <p className="text-gray-700">123 Architectural Avenue, City Name</p>
-                            </div>
-                        </div>
-                        <hr className="w-full h-1 border-black"/>
-                        <div className="flex items-center mb-4"> 
-                            <CiPhone className="text-4xl text-gray-700 mr-4" />
-                            <div>
-                                <p className="text-gray-700">02-xxx-xxxx</p>
-                                <p className="text-gray-700">08x-xxx-xxxx</p>
-                            </div>
-                        </div>
-                        <hr className="w-full h-1 border-black"/>
-                        <div className="flex items-center mb-4"> 
-                            <CiMail className="text-4xl text-gray-700 mr-4" />
-                            <div>
-                                <h3 className="text-lg">PaperPlaneExample@gmail.com</h3>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    {/* Contact Form */}
-                    <div className="bg-gray-100 rounded-lg shadow-lg p-6">
-                        <form>
-                            <div className="mb-4 grid grid-cols-1 sm:grid-cols-2 gap-4"> 
-                                <div>
-                                    <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name*</label>
-                                    <input type="text" id="firstName" className="bg-gray-100 mt-1 focus:outline-none focus:border-b focus:border-green-500 focus:bg-gray-300 block w-full shadow-sm sm:text-sm border-b border-blue-500 rounded-none" />
-                                </div>
-                                <div>
-                                    <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name*</label>
-                                    <input type="text" id="lastName" className="bg-gray-100 mt-1 focus:outline-none focus:border-b focus:border-green-500 focus:bg-gray-300 block w-full shadow-sm sm:text-sm border-b border-blue-500 rounded-none" />
-                                </div>
-                            </div>
-                            <div className="mb-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
-                                <div>
-                                    <label htmlFor="phone" className="block text-sm font-medium text-gray-700">Phone*</label>
-                                    <input type="tel" id="phone" className="bg-gray-100 mt-1 focus:outline-none focus:border-b focus:border-green-500 focus:bg-gray-300 block w-full shadow-sm sm:text-sm border-b border-blue-500 rounded-none" />
-                                </div>
-                                <div>
-                                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email*</label>
-                                    <input type="email" id="email" className="bg-gray-100 mt-1 focus:outline-none focus:border-b focus:border-green-500 focus:bg-gray-300 block w-full shadow-sm sm:text-sm border-b border-blue-500 rounded-none" />
-                                </div>
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="company" className="block text-sm font-medium text-gray-700">Company</label>
-                                <input type="text" id="company" className="bg-gray-100 mt-1 focus:outline-none focus:border-b focus:border-green-500 focus:bg-gray-300 block w-full shadow-sm sm:text-sm border-b border-blue-500 rounded-none" />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
-                                <textarea id="message" className="bg-gray-100 mt-1 focus:outline-none focus:border-b focus:border-green-500 focus:bg-gray-300 block w-full shadow-sm sm:text-sm border-b border-blue-500 rounded-none resize-none h-32"></textarea>
-                            </div>
-                            <button type="submit" style={submitButtonStyle} onMouseEnter={(e) => {
-                                        e.target.style.backgroundColor = 'black';
-                                        e.target.style.color = 'white';
-                                    }} 
-                                    onMouseLeave={(e) => {
-                                        e.target.style.backgroundColor = 'white';
-                                        e.target.style.color = 'black';
-                                    }}>Submit</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            
-            {/* Google Map */}
-            <div className="  mt-8 flex justify-center items-center">
-                {/* Replace with your Google Map */}
-                <div style={{ width: '80%', height: '500px', backgroundColor: 'lightgray' }}>
-                    <h1>Google Map Placeholder</h1>
-                </div>
+                <form onSubmit={handleSubmit} className="ml-6 form-with-divider">
+                    <Container className='left-aligned'>
+                        <hr />
+                        <Row>
+                            <Col md={6} className="info-column">
+                                <Row className="mb-3 align-items-center">
+                                    <Col sm={2}>
+                                        <label className="form-label mt-[-2]">Location</label>
+                                    </Col>
+                                    <Col sm={9}>
+                                        <label className="form-label mt-2">
+                                            8 T-one Building, 12th floor, Sukhumvit 40 Rd., Phra Khanong<br />
+                                            Sub-Districts, Klong toei District, Bangkok 10110.
+                                        </label>
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3 align-items-center">
+                                    <Col sm={2}>
+                                        <label className="form-label mt-2">Tel.</label>
+                                    </Col>
+                                    <Col sm={10}>
+                                        <label className="form-label mt-2">+66 64 789 4428</label>
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3 align-items-center">
+                                    <Col sm={2}>
+                                        <label className="form-label mt-2">Email</label>
+                                    </Col>
+                                    <Col sm={10}>
+                                        <label className="form-label mt-2">paperplane.studio@paperplanebkk.com</label>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col md={6} className="additional-content">
+                                <Row className="mt-4">
+                                    <Col className="text-header left-aligned ml-4">
+                                        <label style={{ fontSize: '36px' }}>We are here to tackle your<br /> questions head-on</label>
+                                    </Col>
+                                </Row>
+                                <Row className='content-divider'> {/* Adds division from here onward */}
+                                    <Col sm={2}>
+                                        <label htmlFor="name" className="form-label mt-2 ml-4">Name</label>
+                                    </Col>
+                                    <Col sm={9}>
+                                        <input type="text" className="form-control ml-4" id="name" name="name" />
+                                    </Col>
+                                </Row>
+                                <Row className='content-divider'>
+                                    <Col sm={2}>
+                                        <label htmlFor="tel" className="form-label ml-4">Tel</label>
+                                    </Col>
+                                    <Col sm={9}>
+                                        <input type="text" className="form-control ml-4" id="tel" name="tel" />
+                                    </Col>
+                                </Row>
+                                <Row className='content-divider'>
+                                    <Col sm={2}>
+                                        <label htmlFor="mail" className="form-label ml-4">Mail</label>
+                                    </Col>
+                                    <Col sm={9}>
+                                        <input type="email" className="form-control ml-4" id="mail" name="mail" />
+                                    </Col>
+                                </Row>
+                                <Row className='content-divider'>
+                                    <Col sm={11}>
+                                        <label htmlFor="tellUs" className="form-label ml-4">Message</label>
+                                        <textarea type="text" className="form-control ml-4" style={{ height: '176.61px' }} id="tellUs" name="tellUs" />
+                                    </Col>
+                                </Row>
+                                <button type="submit" className="btn-submit float-end mt-8" variant="none">
+                                    <span className="ml-6">SUBMIT</span><SlArrowRight className="icon-right" />
+                                </button>
+                            </Col>
+                        </Row>
+                        <hr />
+                    </Container>
+                </form>
             </div>
         </div>
     );
