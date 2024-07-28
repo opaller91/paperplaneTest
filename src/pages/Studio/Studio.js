a4c4e25c41044ec7ffb87a384438dc762c415168
opaller91 <rinrada.ti@ku.th>
2024-07-28 13:43:09 +0700

    [FIX] responsive Studio Page

--- src/pages/Studio/Studio.js
+++ src/pages/Studio/Studio.js
@@ -16,8 +16,8 @@ function Studio() {
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus venenatis velit, a tincidunt elit pulvinar eget. Duis ac ipsum quis lacus interdum tempor. Curabitur auctor nulla ut scelerisque porta. Etiam suscipit congue eros, in interdum risus elementum bibendum. Sed viverra eget nulla a molestie. Maecenas consequat mauris eros, sit amet maximus dui semper vel. Ut auctor, sem rhoncus pulvinar aliquam, felis risus aliquet massa, at tempus ipsum est nec ante. Integer ac tincidunt justo. Vestibulum rutrum enim lacus, sit amet porta lacus pharetra id. Quisque sed lorem metus. Nunc at malesuada arcu, in aliquet ex. Quisque nec enim ac purus auctor aliquam. Nulla suscipit nunc enim, ac varius felis porta a. Aenean et eleifend sapien.</p>
                         </div>
                     </Col>
-                    <Col md={7} className="d-flex justify-content-center align-items-center">  {/* The remaining space */}
-                        <div style={{ width: '949.39px', height: '865.48px' }}>
+                    <Col md={7} className="d-flex justify-content-center align-items-center">
+                        <div className='studio-image'>
                             <img src='/assets/images/ProjectPic5.png' alt="Detail Image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
                         </div>
                     </Col>
